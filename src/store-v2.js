5b07ddfd155cf1686cff6efd1f89e2d736242d6d
Dilmurod Buyukov <dilmurod.buyukovv@gmail.com>
2024-05-05 00:29:07 +0500

    Redux

---
+++ src/store-v2.js
@@ -0,0 +1,18 @@
+import { flushSync } from "react-dom";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
+
+import { thunk } from "redux-thunk";
+
+const rootReducer = combineReducers({
+    account: accountReducer,
+    customer: customerReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(thunk)); // 
+
+export default store;
+
+
+
